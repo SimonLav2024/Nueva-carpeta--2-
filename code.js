8115d1dde0140723c07eec24371dd5fefddf9dac
SimonLav2024 <simonlav94@gmail.com>
2024-11-01 12:45:57 +0100

    terminado

---
+++ code.js
@@ -0,0 +1,30 @@
+
+var qrcode = new QRCode("qrcode");
+const botonQR = document.getElementById("botonQR");
+
+botonQR.addEventListener("click", () => {
+    function makeCode () {    
+        var elText = document.getElementById("text");
+        
+        if (!elText.value) {
+          alert("Input a text");
+          elText.focus();
+          return;
+        }
+        
+        qrcode.makeCode(elText.value);
+      }
+      
+      makeCode();
+      
+      $("#text").
+        on("blur", function () {
+          makeCode();
+        }).
+        on("keydown", function (e) {
+          if (e.keyCode == 13) {
+            makeCode();
+          }
+        });
+});
+
